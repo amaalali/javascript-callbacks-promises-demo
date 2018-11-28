59b9dd8ac3200674a559bae3a54786d753186557
Amaal Ali <amq.ali@gmail.com>
2018-11-28 22:25:46 +0000

    Work on 02 node callback demo.

---
+++ 02-intro-to-callback-concept/02-01-node-using-function-that-takes-a-callback.js
@@ -0,0 +1,32 @@
+/*
+  (1)
+  Description: This demo shows a typical way that an 
+  asyc function can give the user controll. In this
+  case `ARandomAsyncFunction` is a function that takes
+  a callback as its first param. That callback must 
+  accept one param which will be a function that will
+  cause some side effects - in this console.log-ging.
+*/
+
+ARandomAsyncFunction((event) => {
+  console.log("First")
+  event() // (2) remember event is a function
+  console.log("Third")
+})
+
+
+/*
+  Description: This function takes a callback that accepts
+  a param.The param is a function that fires an event when
+  called.
+
+  Note: The code below represents code that you consume.
+  You don't own it and cannot make changes to it.
+*/
+function ARandomAsyncFunction(callback) {
+  setTimeout(() => {
+    callback(
+      () => console.log("Second - In setTimeout")  
+    )
+  }, 1000)
+}
